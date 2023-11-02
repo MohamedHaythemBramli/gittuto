46388ad372c67b5576953e1a6d32073a3488b7b6
MohamedHaythemBramli <mohamedhaythembramli90@gmail.com>
2023-11-02 22:57:20 +0100

    Initial Commit

---
+++ src/main/java/com/example/gittuto/GitTutoApplication.java
@@ -0,0 +1,13 @@
+package com.example.gittuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTutoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitTutoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/gittuto/GitTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gittuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTutoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
